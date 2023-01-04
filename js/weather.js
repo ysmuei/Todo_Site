9421130f1ca8dc04cc482f26c1d813a2c2994f6d
ysmuei <108169494+ysmuei@users.noreply.github.com>
2023-01-05 01:55:36 +0900

    d

--- js/weather.js
+++ js/weather.js
@@ -3,8 +3,16 @@ const API_KEY = "cc6a2ede12eb631541b8d083d4b9957c";
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    console.log(url);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            const name = data.name;
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
 }
 
 function onGeoError(){
