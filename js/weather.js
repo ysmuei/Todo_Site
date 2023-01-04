4a37a715afa01535d6c47e963c8d8e3724320989
ysmuei <108169494+ysmuei@users.noreply.github.com>
2023-01-05 01:20:38 +0900

    input

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,10 @@
+const APT_KEY = "cc6a2ede12eb631541b8d083d4b9957c";
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log("You live it");
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    console.log("url");
 }
 
 function onGeoError(){
